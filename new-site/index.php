f43fcee53ae90ca82d75878d93a4db2d1e84f2b3
Aditya Bodhankar <abaditya23@users.noreply.github.com>
2021-04-20 14:24:03 +0530

    layout implemented

---
+++ new-site/index.php
@@ -0,0 +1,22 @@
+<?php include "./header.php"; ?>
+
+<!-- main code here -->
+<section id="home">
+
+<center>
+    <h2 class="font-styled-header">
+        COMPUTER DEPARTMENT PRESENTS
+    </h2>
+    <h1>
+        <img src="./images/shield_logo.png" alt="Shield Image Logo here" style="max-width: 200px; max-height: 200px;"><br> <!-- TODO:replace with shield new image -->
+       <img src="./images/shield_title.png" alt="Shield" style="max-width: 300px;">
+    </h1>
+    <br>
+    <h5>
+        [Date to be announced]
+    </h5>
+</center>
+
+</section>
+
+<?php include "./footer.php"; ?>
