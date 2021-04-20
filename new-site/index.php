d460e285d1a81b2c0feb9de1690c9ed400f534b7
Aditya Bodhankar <abaditya23@users.noreply.github.com>
2021-04-20 15:09:10 +0530

    .

--- new-site/index.php
+++ new-site/index.php
@@ -1,21 +1,51 @@
 <?php include "./header.php"; ?>
 
+
+<style>
+    .title{
+    background-image: url("./images/shield_logo_bg_only.png");
+    /* Used if the image is unavailable */
+    background-position: center;
+    /* Center the image */
+    background-repeat: no-repeat;
+    /* Do not repeat the image */
+    background-size: 400px;
+    /* Resize the background image to cover the entire container */
+    color: #FFFFFF;
+    min-height: 400px;
+    }
+</style>
+
+
 <!-- main code here -->
 <section id="home">
 
-<center>
-    <h2 class="font-styled-header">
-        COMPUTER DEPARTMENT PRESENTS
-    </h2>
-    <h1>
-        <img src="./images/shield_logo.png" alt="Shield Image Logo here" style="max-width: 200px; max-height: 200px;"><br> <!-- TODO:replace with shield new image -->
-       <img src="./images/shield_title.png" alt="Shield" style="max-width: 300px;">
-    </h1>
-    <br>
-    <h5>
-        [Date to be announced]
-    </h5>
-</center>
+    <center>
+        
+        <h1 class="font-styled-header">
+            COMPUTER DEPARTMENT PRESENTS
+        </h1>
+        <div class="title"><br><br><br><br><br>
+            <h1>
+                <!-- <img src="./images/shield_logo.png" alt="Shield Image Logo here" style="max-width: 200px; max-height: 200px;"><br> TODO:replace with shield new image -->
+                <img src="./images/shield_title.png" alt="Shield" style="max-width: 300px;">
+                <br>
+                4.0
+            </h1>
+            <h5>
+                VISION&nbsp; TO&nbsp; REALITY
+            </h5>
+            <br>
+            <h5>
+                [Date to be announced]
+            </h5>
+        </div>
+
+        <h3 class="font-styled-header">
+            SPONSORED BY
+        </h3>
+        <div class="container" id="sponsors"></div>
+    </center>
 
 </section>
 
