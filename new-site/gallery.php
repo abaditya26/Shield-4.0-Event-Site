ff96773815d307fe119217f28ad3d37bf0d71782
Aditya Bodhankar <abaditya23@users.noreply.github.com>
2021-04-20 19:17:08 +0530

    gallery

--- new-site/gallery.php
+++ new-site/gallery.php
@@ -7,17 +7,90 @@ $imageData = [
         "Shield BAckground"
     ],
     [
-        "./images/gpj_logo.png",
-        "GPJ Logo"
+        "./images/gallery/1.jpg",
+        "Shield BAckground"
     ],
     [
-        "./images/shield_logo.png",
-        "Shield Logo"
+        "./images/gallery/2.jpg",
+        "Shield BAckground"
     ],
     [
-        "./images/shield_title.png",
-        "Shield Title"
-    ]
+        "./images/gallery/3.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/4.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/5.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/6.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/7.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/8.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/9.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/10.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/11.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/12.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/13.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/14.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/15.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/16.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/17.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/18.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/19.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/20.jpg",
+        "Shield BAckground"
+    ],
+    [
+        "./images/gallery/21.jpg",
+        "Shield BAckground"
+    ],
+    
 ]
 ?>
 
@@ -29,7 +102,7 @@ $imageData = [
         ?>
             <div class="col-md-4">
                 <center>
-                    <img src="<?php echo $imageData[$i][0]; ?>" alt="<?php echo $imageData[$i][1]; ?>" style="max-width: 100%; max-height: 300px;">
+                    <img src="<?php echo $imageData[$i][0]; ?>" alt="<?php echo $imageData[$i][1]; ?>" style="max-width: 100%; max-height: 250px; border: ridge; margin:10px;">
                 </center>
             </div>
         <?php
