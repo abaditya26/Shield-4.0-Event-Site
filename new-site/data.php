b25e0443ff59c5bd664c3149639c3053084e50fa
Mahesh Pimparkar <43509522+maheshzip@users.noreply.github.com>
2021-04-23 19:00:40 +0530

    .

--- new-site/data.php
+++ new-site/data.php
@@ -5,49 +5,42 @@ $eventData = [
         "Virtual Treasure Hunt",
         "./images/events/1.gif",
         "The Description Of the event goes here"
-        "./docs/tre_hunt.pdf",
     ],
     [
         "CQuiz",
         "C Quiz",
         "./images/events/c.gif",
         "The Description Of the event goes here"
-        "./docs/tre_hunt.pdf",
     ],
     [
         "GkQuiz",
         "GK Quiz",
         "./images/events/gk.gif",
         "The Description Of the event goes here"
-        "./docs/tre_hunt.pdf",
     ],
     [
         "ppt",
         "PPT/Poster Presentation",
         "./images/events/ppt.gif",
         "The Description Of the event goes here"
-        "./docs/tre_hunt.pdf",
     ],
     [
         "project",
         "Project Copetition",
         "./images/events/project.gif",
         "The Description Of the event goes here"
-        "./docs/tre_hunt.pdf",
     ],
     [
         "chess",
         "Online Chess",
         "./images/events/chess.gif",
         "The Description Of the event goes here"
-        "./docs/tre_hunt.pdf",
     ],
     [
         "talent",
         "Talent Hunt",
         "./images/events/talhunt.gif",
         "The Description Of the event goes here"
-        "./docs/tre_hunt.pdf",
     ],
 ];
 ?>
