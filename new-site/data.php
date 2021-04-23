3aa26520ad35a7cf15b8e9fcfd56377aff00438a
Mahesh Pimparkar <43509522+maheshzip@users.noreply.github.com>
2021-04-23 21:06:09 +0530

    please stay away from data.php im working on it

--- new-site/data.php
+++ new-site/data.php
@@ -5,7 +5,8 @@ $eventData = [
         "Virtual Treasure Hunt",
         "./images/events/1.gif",
         "./docs/tre_hunt.pdf",
-        "The Description Of the event goes here",
+        "The Game totally depends on your ability to search things on internet. \n Find Codes Crack Codes And Win! Game based on CTF (CAPTURE THE FLAG).
+        You have to find passwords and decode some codes.This activity are also known as an Online Scavenger Hunt, Digital Scavenger Hunt or Virtual Treasure Hunt. ",
         "Coordinators:-\n<ul><li>Mahesh Pimparkar</li><li>Shreya Agrawal</li></ul>Contact No:-<a href=\"tel:+918975311578\">+918975311578</a>"
     ],
     [
@@ -13,24 +14,26 @@ $eventData = [
         "C Quiz",
         "./images/events/c.gif",
         "./docs/c_quiz.pdf",
-        "The Description Of the event goes here",
-        "Coordinators:-\n<ul><li>Mahesh Pimparkar</li><li>Shreya Agrawal</li></ul>Contact No:-<a href=\"tel:+918975311578\">+918975311578</a>"  
+        "The game is totally based on c programming knowledge of participant. 30 MCQ (Multiple Choice Question) in 30 minutes. In the case of tie then maximum score and minimum time taken for solving all the questions will considered. Solve all the questions in minimum time and win prizes! ",
+        "Coordinators:-\n<ul><li>Kalpak Nemade</li><li>Mahesh Pimparkar</li></ul>Contact No:-<a href=\"tel:+9173050103025\">+9173050103025</a>"  
     ],
     [
         "GkQuiz",
         "GK Quiz",
         "./images/events/gk.gif",
         "./docs/About_gk.pdf",
-        "The Description Of the event goes here",
-        "Coordinators:-\n<ul><li>Mahesh Pimparkar</li><li>Shreya Agrawal</li></ul>Contact No:-<a href=\"tel:+918975311578\">+918975311578</a>"  
+        "The Game totally depends on the intelligence/GK of the participants.
+        There are 2 rounds of this game. First round will be of Online MCQ (Multiple Choice Question) which is fully based on general knowledge , current affairs, etc. ",
+        "Coordinators:-\n<ul><li>Komal Dhake</li><li>Damini Dhangar</li></ul>Contact No:-<a href=\"tel:+919370440747\">+919370440747    </a>"  
     ],
     [
         "ppt",
         "PPT/Poster Presentation",
         "./images/events/ppt.gif",
         "./docs/ppt.pdf",
-        "The Description Of the event goes here",
-        "Coordinators:-\n<ul><li>Mahesh Pimparkar</li><li>Shreya Agrawal</li></ul>Contact No:-<a href=\"tel:+918975311578\">+918975311578</a>"  
+        "The competition mainly focuses on your speaking skills and your way of
+        presentation on any of the given topics.You will be given merit points on the basis of your presentation. Participants with most merit points will be declared winner by the authorities. PPTs will be given more precedence than Posters. ",
+        "Coordinators:-\n<ul><li>Ajinkya Mahajan</li><li>Rakshanda Giri</li></ul>Contact No:-<a href=\"tel:+917666124625\">+917666124625</a>"  
     ],
     [
         "project",
