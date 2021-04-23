bdb52cbffbe8b58b2838e25f655fb554bfdefd35
Mahesh Pimparkar <43509522+maheshzip@users.noreply.github.com>
2021-04-23 18:55:00 +0530

    pdf included to download instrctions

--- new-site/data.php
+++ new-site/data.php
@@ -4,42 +4,49 @@ $eventData = [
         "treasure",
         "Virtual Treasure Hunt",
         "./images/events/1.gif",
+        "./docs/tre_hunt.pdf",
         "The Description Of the event goes here"
     ],
     [
         "CQuiz",
         "C Quiz",
         "./images/events/c.gif",
+        "./docs/tre_hunt.pdf",
         "The Description Of the event goes here"
     ],
     [
         "GkQuiz",
         "GK Quiz",
         "./images/events/gk.gif",
+        "./docs/tre_hunt.pdf",
         "The Description Of the event goes here"
     ],
     [
         "ppt",
         "PPT/Poster Presentation",
         "./images/events/ppt.gif",
+        "./docs/tre_hunt.pdf",
         "The Description Of the event goes here"
     ],
     [
         "project",
         "Project Copetition",
         "./images/events/project.gif",
+        "./docs/tre_hunt.pdf",
         "The Description Of the event goes here"
     ],
     [
         "chess",
         "Online Chess",
         "./images/events/chess.gif",
+        "./docs/tre_hunt.pdf",
         "The Description Of the event goes here"
     ],
     [
         "talent",
         "Talent Hunt",
         "./images/events/talhunt.gif",
+        "./docs/tre_hunt.pdf",
         "The Description Of the event goes here"
     ],
 ];
