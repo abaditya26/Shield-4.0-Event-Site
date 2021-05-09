f80981cea3a85191a3da00bb5b582a1d4e667a43
maheshzip <maheshpimparkar6@gmail.com>
2021-05-09 13:38:30 +0530

    .

--- flagsub.php
+++ flagsub.php
@@ -3,38 +3,22 @@
 
 
 <div class="container" style="max-width:600px;" >
+<!-- PASSWORD FOR PDF1 IS "1578" -->
+
 
 <center>
 
 <h2>VIRTUAL TREASURE HUNT</h2>
-<p>Select Flag number From DropDown Menu and Submit Code.</p><br>
-
-<select class="form-control" aria-label="Default select example" id="flagno" >
-  
-
-  <option selected>Select FLAG</option>
-  <option value="1">FLAG NO 1</option>
-  <option value="2">FLAG NO 2</option>
-  <option value="3">FLAG NO 3</option>
-  <option value="3">FLAG NO 4</option>
-  <option value="3">FLAG NO 5</option>
-  <option value="3">FLAG NO 6</option>
-  <option value="3">FLAG NO 7</option>
-  <option value="3">FLAG NO 8</option>
-  <option value="3">FLAG NO 9</option>
-  <option value="3">FLAG NO 10</option>
-
-</select>
-<br>
-
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">FLAG CODE</label>
-  <input type="text" class="form-control" id="flagcode" placeholder="Enter FLAG CODE">
-</div>
-<br>
+<p>DOWNLOAD CRACKME.ZIP AND INSTRUTIONS.TXT HERE.</p><br>
 
 
-<button type="button" class="btn btn-primary">Submit FLAG</button>
+<button type="button" class="btn btn-outline-success" a href="crackme.zip" ><b>CRACKME.ZIP</b></button>
+<button type="button" class="btn btn-outline-danger" a href="http://shield2k2.ml/instructions.txt" ><b>INSTRUTIONS.TXT</b></button><br>
+
+
+<br><p>Code never lies, notes sometimes do. 👍</p><br>
+
+
 
 </center>
 
