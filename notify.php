ad7841faa3894394f3387f420544e155d5e14b36
maheshzip <maheshpimparkar6@gmail.com>
2021-05-09 14:01:06 +0530

    okay

---
+++ notify.php
@@ -0,0 +1,19 @@
+<?php include "./header.php"; ?>
+
+
+
+<div class="container" style="max-width:600px;" >
+
+<center>
+<h2>Notifications</h2>
+<p>Bookmark this page for upcoming Notificatons and Results.</p>
+
+</center>
+<p>09 MAY 2021 Tentative schedule for event </p>
+
+
+</div>
+ 
+
+
+<?php include "./footer.php"; ?>
