49c1f1e633492d0392ff6e76e2f5516f9579cdcd
Aditya Bodhankae <adityaabodhankar@gmail.com>
2021-05-13 08:51:09 +0530

    .

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php include "./header.php"; ?>
+
+<div class="container" style="margin-top: 10%; margin-bottom: 10%;">
+    <center>
+        <h2>
+            Page Not Found
+        </h2>
+    </center>
+</div>
+
+
+<?php include "./footer.php"; ?>
